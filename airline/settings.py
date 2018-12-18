ffa962bc05ef8e79114e53a0d0d94098037f8fc0
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2018-12-18 19:58:52 -0200

    Adding django_heroku to setting files

--- airline/settings.py
+++ airline/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '7!@8iekdn*_6e&x8m!mofryeaw775c&%-nc_)z)+p9w5@bw+(n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://vdhug-airline-travis.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
